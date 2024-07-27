ea7179ebb8a32892ff96b1c736e0d95c54d5b647
JuanSe <juan_gonzalez82181@elpoli.edu.co>
2024-07-26 23:37:46 -0500

    ✨implements options in yargs plugin

--- src/plugins/args.plugin.ts
+++ src/plugins/args.plugin.ts
@@ -1,4 +1,4 @@
-import yargs, { options } from 'yargs';
+import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers'; 
 
 export const yarg = yargs(hideBin(process.argv))
@@ -7,5 +7,24 @@ export const yarg = yargs(hideBin(process.argv))
         type: 'number',
         demandOption: true,
         describe: 'Multiplication table base'
+    })
+    .option('l', {
+        alias: 'limit',
+        type: 'number',
+        default: 10,
+        describe: 'Multiplication table limit'
+    })
+    .option('s', {
+        alias: 'show',
+        type: 'boolean',
+        default: false,
+        describe: 'Show multiplication table'
+    })
+    .check((argv, options) => {
+
+        if (argv.b < 1 ) throw 'ERROR: base must be greater than 0';
+
+        return true;
+
     })
     .parseSync()
