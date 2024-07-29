d0c68afae551cff8add02827e3a2d516c52fb30b
JuanSe <juan_gonzalez82181@elpoli.edu.co>
2024-07-28 20:38:05 -0500

    🐛fix a bug in cycle for

--- src/domain/use-cases/create-table.use-case.ts
+++ src/domain/use-cases/create-table.use-case.ts
@@ -19,9 +19,12 @@ export class CreateTable implements CreateTableUseCase {
     execute({ base, limit = 10 }: CreateTableOptions ){
 
         let outputMessage = '';
-        for (let i = 0; i < limit; i++) {
-            outputMessage += `${ base } x ${i} = ${ base * i }\n`;
+        for (let i = 1; i <= limit; i++) {
+            outputMessage += `${ base } x ${i} = ${ base * i }`;
+            
+            if (i < limit ) outputMessage += '\n'; 
         }
+
 
         return outputMessage;
     };
