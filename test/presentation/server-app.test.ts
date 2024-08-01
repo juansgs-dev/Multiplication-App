31d309b68b8e75fc7423da62e4b7dacd1b26660e
JuanSe <juan_gonzalez82181@elpoli.edu.co>
2024-08-01 05:22:55 -0500

    🧪add Spy createTable and logSpy in test 'should run ServerApp with options'

--- test/presentation/server-app.test.ts
+++ test/presentation/server-app.test.ts
@@ -1,7 +1,18 @@
 import { ServerApp } from '../../src/presentation/server-app';
+import { CreateTable } from '../../src/domain/use-cases/create-table.use-case';
+import { SaveFile } from '../../src/domain/use-cases/save-file.use-case';
 
 
 describe('Server App', () => {
+
+    const options = {
+        base: 2,
+        limit: 5,
+        showTable: false,
+        destination: 'test-destination',
+        name: 'test-filename'
+    }
+
 
     test('should create ServerApp instance', () => {
 
@@ -15,21 +26,26 @@ describe('Server App', () => {
     test('should run ServerApp with options', () => {
 
         const logSpy = jest.spyOn(console, 'log');
-
-
-        const options = {
-            base: 2,
-            limit: 5,
-            showTable: false,
-            destination: 'test-destination',
-            name: 'test-filename'
-        }
+        const createTableSpy = jest.spyOn( CreateTable.prototype, 'execute' );
+        // const saveFileSpy = jest.spyOn( SaveFile.prototype, 'execute');
 
         ServerApp.run(options);
 
         expect(logSpy).toHaveBeenCalledTimes(2);
         expect(logSpy).toHaveBeenCalledWith('Server running...');
         expect(logSpy).toHaveBeenLastCalledWith('File Created');
+
+        expect(createTableSpy).toHaveBeenCalledTimes(1);
+        expect(createTableSpy).toHaveBeenCalledWith({
+            base: options.base, limit: options.limit
+        });
+
+        // expect(saveFileSpy).toHaveBeenCalledTimes(1);
+        // expect(saveFileSpy).toHaveBeenCalledWith({
+        //     fileContent: expect.any(String),
+        //     fileName: options.name,
+        //     fileDestination: options.destination
+        // });
 
     })
 
