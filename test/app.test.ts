db8fa2ced952ed5d0cb92c8622cdd2b8d5a8c280
JuanSe <juan_gonzalez82181@elpoli.edu.co>
2024-08-08 20:22:21 -0500

    🧪 add expect in test 'should call server.run with values'

--- test/app.test.ts
+++ test/app.test.ts
@@ -10,8 +10,13 @@ describe('Test app.ts', () => {
         process.argv = ['node', 'app.ts', '-b', '10', '-l', '5', '-s', '-n', 'test-file', '-d', 'test-destination'];
         await import('../src/app');
 
-        
-
+        expect(serverRunMock).toHaveBeenCalledWith({
+            base: 10,
+            limit: 5,
+            showTable: true,
+            name: 'test-file',
+            destination: 'test-destination'
+        });
     });
 
 });
